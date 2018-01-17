166100654b645a82712a6f9f13680f20b7f6af51
Kayla Reopelle <kreopelle@gmail.com>
2018-01-17 20:50:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function theBeatlesPlay(arrayMusicians, arrayInstruments) {
+  var emptyArray = [];
+  for (let i = 0; i < arrayMusicians.length; i++) {
+     emptyArray.push(`${arrayMusicians[i]} plays ${arrayInstruments[i]}`);
+ }
+ return emptyArray;
+}
+
+function johnLennonFacts (array) {
+  let i = 0
+  while (i < array.length) {
+    array[i] = array[i] + "!!!";
+    i++
+  }
+  return array;
+}
+
+function iLoveTheBeatles(n) {
+  var newArray = [];
+  do {
+    newArray.push("I love the Beatles!");
+    n++;
+  } while (n < 15);
+  return newArray;
+}
